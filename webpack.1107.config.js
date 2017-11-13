c375a22bc27b361491794e7337100fa56ae4c60e
cindy2011 <weimengmeng2011@163.com>
2017-11-13 10:12:10 +0800

    add CommonsChunkPlugin

---
+++ webpack.1107.config.js
@@ -0,0 +1,66 @@
+const Path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack=require("webpack");
+
+module.exports = {
+    entry: {
+        index: Path.resolve(__dirname, "src/main.js"),
+        vendors:['vue','vue-router','moment','axios']
+    },
+    output: {
+        path: Path.resolve(__dirname, "dist"),
+        filename: "[name]_bundle.js?v=[chunkhash:8]"
+    },
+    module: {
+        loaders: [{
+            test: /\.vue$/,
+            include: Path.resolve(__dirname, "src"),
+            loader: "vue-loader"
+        }, {
+            test: /\.css$/,
+            //loader: "style-loader!css-loader!autoprefixer-loader"
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader!autoprefixer-loader"
+            })
+        }, {
+            test: /\.js$/,
+            loader: "babel-loader",
+            include: Path.resolve(__dirname, "src")
+        }, {
+            test: /\.(jpg|gif|svg|png|ttf)$/,
+            loader: "url-loader",
+            include: Path.resolve(__dirname, "src"),
+            options: {
+                limit: 409600,
+                name: "[name].[ext]"
+            }
+        }]
+    },
+    resolve: {
+        extensions: [".js", ".vue"],
+        alias: {
+            "vue$": "vue/dist/vue.esm.js"
+        }
+    },
+    plugins: [
+        //new ExtractTextPlugin("[contenthash].css"),
+        new ExtractTextPlugin({
+            filename: (getPath) => {
+                return getPath('css/[name].css?v=[contenthash:8]');
+            },
+            allChunks: true
+        }),
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: Path.resolve(__dirname, "src/index.html"),
+            inject: true
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            names:["vendors","mainfest"]
+        })
+    ]
+}
